6e77fe0b52030b79a37fcc9690d8fba5c3a5923f
junseuk <junseuk86@gmail.com>
2021-09-22 18:24:38 +0900

    png response

--- httpserver.js
+++ httpserver.js
@@ -1,9 +1,16 @@
 var http = require('http');
-var server = http.createServer();
+var fs = require('fs');
 
-server.on('request', function(req, res) {
-    res.write('Hello world');
-    res.end();
-});
 
-server.listen(3000);
+var server = http.createServer(function(req, res) {
+    fs.access('./cat2.png', function(err) {
+        if (err) {
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        fs.readFile('./cat2.png', function(err, data){
+            res.end(data);
+        });
+    });
+}).listen(3000);
