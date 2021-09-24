092b0fb2c0a0115b71df62d7a10a674b68282af1
junseuk <junseuk86@gmail.com>
2021-09-24 14:32:38 +0900

    post res, req handle

--- httpserver.js
+++ httpserver.js
@@ -1,16 +1,57 @@
 var http = require('http');
-var fs = require('fs');
+var querystring = require('querystring');
+
+var movieList = [{ title: 'Starwars', director: '조지루카스'}];
 
 
 var server = http.createServer(function(req, res) {
-    fs.access('./cat2.png', function(err) {
-        if (err) {
-            res.statusCode = 404;
-            res.end();
-            return;
-        }
-        fs.readFile('./cat2.png', function(err, data){
-            res.end(data);
-        });
-    });
+    if (req.method.toLowerCase() == 'post') {
+        addNewMovie(req, res);
+    }
+    else {
+        showList(req, res);
+    }
 }).listen(3000);
+
+function showList(req, res) {
+    res.writeHeader(200, {'Content-Type': 'text/html; charset=UTF-8'});
+    res.write('<html>');
+    res.write('<meta charset="UTF-8">');
+    res.write('<body>');
+
+    res.write('<h3>Favorite Movie</h3>');
+    res.write('<div><ul>');
+
+    movieList.forEach(function (item){
+        res.write('<li>'+item.title + '(' + item.director + ')</li>');
+    }, this);
+    res.write('</ul></div>');
+
+    res.write(
+        '<form method="post" action="."><h4>새 영화 입력</h4>' +
+        '<div><input type="text" name="title" placeholder="영화제목"></input></div>' +
+        '<div><input type="text" name="director" placeholder="감독"></input></div>' +
+        '<input type="submit" value="upload"></input></form>'
+    );
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+}
+function addNewMovie(req, res) {
+    var body = '';
+    req.on('data', function(chunk){
+        body += chunk;
+    });
+    req.on('end', function() {
+        var data = querystring.parse(body);
+        var title = data.title;
+        var director = data.director;
+
+        movieList.push({title:title, director:director});
+        //Redirect 처리
+        res.statusCode = 302;
+        res.setHeader('Location', '.');
+        res.end();
+    })
+
+}
