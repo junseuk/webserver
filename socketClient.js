263a75c856bd16656a711b1cd8994178797ca91d
junseuk <junseuk86@gmail.com>
2021-09-28 00:17:58 +0900

    simple tcp socket connection

--- socketClient.js
+++ socketClient.js
@@ -6,15 +6,15 @@ var port = 3000;
 var socket = new net.Socket();
 socket.connect({host:host, port:port}, function(){
     console.log('success on connection')
-    socket.on('data', function(){
+    socket.on('data', function(data){
         var str = data.toString();
         console.log('>>', str);
     });
+    socket.write('Hello Socket Server');
+    socket.write('bye bye');
     socket.on('end', function(){
         console.log('connection ends');
     });
-    socket.write('Hello Socket Server');
-    socket.write('bye bye');
 
     socket.end();
 })
